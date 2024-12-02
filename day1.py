b001c218693485a6510b26dffb31e015890d6a48
raxleys <raxleys@gmail.com>
2024-12-01 19:38:20 -0500

    Add solution for day 1

---
+++ day1.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+# Part 1
+with open("input-day1.txt") as f:
+    lines = [l.strip() for l in f.readlines()]
+
+firsts = []
+seconds = []
+for l in lines:
+    f, s = [w for w in l.split(" ") if w != ""]
+    firsts.append(int(f))
+    seconds.append(int(s))
+
+firsts.sort()
+seconds.sort()
+
+tot = 0
+for i in range(len(firsts)):
+    tot += abs(seconds[i] - firsts[i])
+
+print(f"Distance is: {tot}")
+
+# Part 2
+occurs = defaultdict(int)
+firsts = []
+for l in lines:
+    f, s = [w for w in l.split(" ") if w != ""]
+    firsts.append(int(f))
+    occurs[int(s)] += 1
+
+sim = 0
+for n in firsts:
+    sim += n * occurs[n]
+    
+print(f"Similarity: {sim}")
+
