a5d1f4c2b0fc81fbbd9da53d5634eccbd9bbb4ad
raxleys <raxleys@gmail.com>
2025-01-26 17:48:13 -0500

    Add solution for day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use utils::Res;
+use utils::{Res, Soln};
 
 mod utils {
+    use crate::SOLNS;
     use std::error::Error;
     use std::fs::File;
     use std::io::{self, BufRead};
@@ -10,24 +11,47 @@ mod utils {
 
     pub struct Args {
         pub input_path: String,
-        pub problem: i32,
+        pub problem: usize,
+        pub subproblem: usize,
+    }
+
+    pub type SolnFn = fn(&Vec<String>) -> Res<i32>;
+    pub struct Soln {
+        pub first: SolnFn,
+        pub second: SolnFn,
+    }
+
+    pub fn dummy_soln(_input: &Vec<String>) -> Res<i32> {
+        unimplemented!("Solution is not implemented");
     }
 
     pub fn parse_args() -> Option<Args> {
         let args: Vec<_> = std::env::args().collect();
-        if args.len() != 3 {
+        if args.len() != 4 {
             print_help(pname(&args[0])?);
             return None;
         }
 
-        if let Ok(num) = args[2].parse::<i32>() {
+        let problem = match args[1].parse::<i32>() {
+            Ok(num) => {
+                if num <= 0 || num as usize > SOLNS.len() {
+                    eprintln!("Only problems from 1 to {} have solutions!", SOLNS.len());
+                    return None;
+                }
+                Some(num as usize)
+            }
+            Err(_) => None,
+        }?;
+
+        if let Ok(num) = args[3].parse::<i32>() {
             if num <= 0 || num > 2 {
-                eprintln!("Only problems 1 or 2 can be selected!");
+                eprintln!("Only sub-problems 1 or 2 can be selected!");
                 return None;
             } else {
                 return Some(Args {
-                    input_path: args[1].to_string(),
-                    problem: num,
+                    input_path: args[2].to_string(),
+                    problem: problem,
+                    subproblem: num as usize,
                 });
             }
         }
@@ -44,7 +68,7 @@ mod utils {
     }
 
     fn print_help(pname: &str) {
-        eprintln!("Usage: {} [INPUT] [1|2]", pname);
+        eprintln!("Usage: {} [PROB #] [INPUT] [1|2]", pname);
     }
 
     pub fn read_file_lines(fname: &str) -> Res<Vec<String>> {
@@ -57,7 +81,7 @@ mod utils {
         Ok(lines)
     }
 
-    pub fn split_columns(input: &Vec<String>) -> Res<(Vec<i32>, Vec<i32>)> {
+    pub fn split_columns_int(input: &Vec<String>) -> Res<(Vec<i32>, Vec<i32>)> {
         let mut first = Vec::new();
         let mut second = Vec::new();
         for line in input.iter() {
@@ -80,14 +104,63 @@ mod utils {
     }
 }
 
+mod day1 {
+    use crate::utils;
+    use utils::{dummy_soln, Res, Soln};
+
+    pub fn ans(input: &Vec<String>) -> Res<i32> {
+        let (mut first, mut second) = utils::split_columns_int(input).map_err(|e| {
+            eprintln!("Failed to split columns: {e}");
+            e
+        })?;
+
+        if first.len() != second.len() {
+            return Err(format!(
+                "Lists have varying lengths!: {} vs {}",
+                first.len(),
+                second.len()
+            )
+            .into());
+        }
+
+        first.sort();
+        second.sort();
+        let sum = first
+            .into_iter()
+            .zip(second.into_iter())
+            .map(|(first, second)| (first - second).abs())
+            .sum();
+
+        Ok(sum)
+    }
+
+    pub const DAY1_SOLN: Soln = Soln {
+        first: ans,
+        second: dummy_soln,
+    };
+}
+
+const SOLNS: &[Soln] = &[day1::DAY1_SOLN];
+
 fn main() -> Res<()> {
     if let Some(args) = utils::parse_args() {
         let prob = args.problem;
         let input = utils::read_file_lines(&args.input_path)?;
-        let (first, second) = utils::split_columns(&input)?;
-        println!("Solving problem: {}", prob);
-        println!("Sum of first: {}", first.iter().sum::<i32>());
-        println!("Sum of second: {}", second.iter().sum::<i32>());
+        let subprob = args.subproblem;
+
+        let soln = &SOLNS[prob - 1];
+        let func = match subprob {
+            1 => soln.first,
+            2 => soln.second,
+            _ => unreachable!(),
+        };
+        println!(
+            "Answer to problem #{} part {}: {}",
+            prob,
+            subprob,
+            func(&input)?
+        );
+
         return Ok(());
     }
 
